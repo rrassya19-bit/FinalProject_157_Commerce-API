4d12799d71092da89932f9d4a5606d1c91cdf376
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-08-24 01:41:53 +0700

    add configuration file for database connection settings in development and production environments

---
+++ backend/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'postgres',
+    database: process.env.DB_DATABASE || 'commerceapi',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    logging: false
+  },
+  production: {
+    use_env_variable: 'POSTGRES_URL',
+    url: process.env.POSTGRES_URL,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
